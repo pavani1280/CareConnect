81b084263fb2eae22b83e76addd6fa9cdc1e9555
pavani1280 <pavani.8021@gmail.com>
2026-09-24 14:47:24 +0530

    configure frontend for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://careconnect-77da.onrender.c',
+        target: 'https://careconnect-77da.onrender.com',
         changeOrigin: true,
       },
     },
