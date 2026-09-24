faf97656c56c89799897f0325cac02366d3b3012
pavani1280 <pavani.8021@gmail.com>
2026-09-24 15:14:31 +0530

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -15,7 +15,10 @@ connectDB();
 
 // Security Middlewares
 app.use(helmet());
-app.use(cors({ origin: '*', credentials: true }));
+app.use(cors({
+  origin: 'https://care-connect-erti.vercel.app',
+  credentials: true,
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
